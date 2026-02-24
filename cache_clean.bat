1e5b361849e31785810ba31c85e32bbfc7f5feef
DPGuide <34819326+DPGuide@users.noreply.github.com>
2026-02-24 15:02:40 +0100

    Add files via upload

--- cache_clean.bat
+++ cache_clean.bat
@@ -1,23 +1,23 @@
 @echo off
-:: Fragt Admin-Rechte ab, falls nicht vorhanden
+:: Fragt Admin-Rechte ab, falls nicht vorhanden / Checks for administrator rights if not already present
 net session >nul 2>&1
 if %errorLevel% == 0 (
-    echo Admin-Rechte bestatigt...
+    echo Admin-Rechte bestatigt / Admin rights confirmed...
 ) else (
-    echo Bitte mit Rechtsklick "Als Administrator ausfuhren"!
+    echo Bitte mit Rechtsklick "Als Administrator ausfuhren / Please right-click and select "Run as administrator"."!
     pause
     exit
 )
 
-echo Leere Benutzer-Temp...
+echo Leere Benutzer-Temp / Empty user temp...
 del /s /f /q %temp%\*.* >nul 2>&1
 
-echo Leere System-Temp...
+echo Leere System-Temp / Empty system temp...
 del /s /f /q C:\Windows\Temp\*.* >nul 2>&1
 
-echo Leere Papierkorb...
+echo Leere Papierkorb / Empty wastebasket...
 rd /s /q %systemdrive%\$Recycle.bin >nul 2>&1
 
 echo.
-echo --- Alles erledigt! Dein PC ist ein Stück sauberer. ---
+echo --- Alles erledigt! Dein PC ist ein Stück sauberer. / All done! Your PC is a bit cleaner. ---
 pause
